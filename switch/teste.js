0720d3a5aa9ca64a213d1acc2b792046e859b837
João Vitor <132713260+J-vVitor@users.noreply.github.com>
2023-11-13 21:46:08 -0300

    atu

---
+++ switch/teste.js
@@ -0,0 +1,12 @@
+let job = ' ';
+
+switch(job){
+    case 'ator':
+        console.log('Você é um ator')
+        break
+    case 'programador':
+        console.log('Você é um programador')
+        break
+    default:
+        console.log('Profissão não identificada')
+}
